017f10c1a0390e46b7c68ea09f981035b12945e4
Nico Francois <nico.francois@dot-i.com>
2012-12-26 21:47:05 +0100

    Initial checkin.

---
+++ ArgusTV.Recorders.Common/PlatformInfo.cs
@@ -0,0 +1,52 @@
+﻿/*
+ *	Copyright (C) 2007-2012 ARGUS TV
+ *	http://www.argus-tv.com
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArgusTV.Recorders.Common
+{
+    public class PlatformInfo
+    {
+        /// <summary>
+        /// Is this an NT platform?
+        /// </summary>
+        public static bool IsWindowsNT
+        {
+            get 
+            { 
+                return (PlatformID.Win32NT == Environment.OSVersion.Platform); 
+            }
+        }
+
+        /// <summary>
+        /// Returns true if this is Windows 2000 or higher
+        /// </summary>
+        public static bool IsWindows2KUp
+        {
+            get
+            {
+                OperatingSystem os = Environment.OSVersion;
+                return (PlatformID.Win32NT == os.Platform && os.Version.Major >= 5);
+            }
+        }
+    }
+}
